856fa6ca5cbf54b71a01eac0c62bd67b30b475c6
idKrazu <idkrazu@gmail.com>
2020-02-17 12:04:20 +0100

    Added calculate function

--- app.js
+++ app.js
@@ -1,32 +1,76 @@
 const result = document.querySelector(".result-box");
-let firstNumber = [];
+let firstNumberArr = []; // * Array with current number
+let firstNumberInt;
+let numbersArr = []; // * Array with every number typed in
+let resultInt;
+let signArr = [];
 
 function init() {
   document
     .querySelector(".main-container")
     .addEventListener("click", function(event) {
       if (event.target.tagName === "BUTTON") {
-        /*  console.log(`Clicked: ${event.target.innerText}`);
-        console.log(event.target.className.includes("number-box")); */
         if (event.target.className.includes("number-box") === true) {
           numberClicked();
+        } else if (event.target.className.includes("operation-box") === true) {
+          operationClicked();
+        } else if (event.target.className.includes("equal-box") === true) {
+          calucate();
         }
       }
     });
 }
 
 function numberClicked() {
-  /* 
- ! zamień to potem ze stringów na inty i przy wyświetlaniu na odwrót :))))))))))
-*/
-  console.log(event.target.innerText);
-  firstNumber.push(event.target.innerText);
-  console.log(firstNumber);
+  // * Keeping track of every number clicked and throwing it in array
+  firstNumberArr.push(event.target.innerText);
+  firstNumberInt = Number(firstNumberArr.join(""));
+  console.log(`Current number ${firstNumberInt}`);
   displayResult();
 }
 
+function operationClicked() {
+  numbersArr.push(firstNumberInt);
+  signArr.push(event.target.innerText);
+  firstNumberArr = [];
+  firstNumberInt = null;
+  console.log(numbersArr);
+  console.log(signArr);
+}
+function calucate() {
+  numbersArr.push(firstNumberInt);
+  firstNumberArr = [];
+  firstNumberInt = null;
+  if (numbersArr.length === 1) {
+    let currentNumber = numbersArr[0];
+    resultInt = currentNumber;
+  } else {
+    for (let i = 0; i < numbersArr.length; i++) {
+      if (i === 0) {
+        let currentNumber = numbersArr[0];
+        resultInt = currentNumber;
+      } else {
+        let currentNumber = numbersArr[i];
+        let currentSign = signArr[i - 1];
+        if (currentSign === "-") {
+          resultInt -= currentNumber;
+        } else if (currentSign === "x") {
+          resultInt *= currentNumber;
+        } else if (currentSign === "+") {
+          resultInt += currentNumber;
+        } else if (currentSign === "÷") {
+          resultInt /= currentNumber;
+        }
+      }
+    }
+  }
+  console.log(`Result: ${resultInt}`);
+}
+
+function equalClicked() {}
+
 function displayResult() {
-  console.log(firstNumber.join);
+  result.innerText = resultInt;
 }
 
 init();
